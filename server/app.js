fe82c417631f5eb9df61c7386c21ae9eb376cf29
Angad Singh <angad.w.s.1994@gmail.com>
2018-05-27 12:27:08 +0530

    Enabled CORS

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const taskRouter = require('./api/routes/tasks');
 
 mongoose.connect("mongodb://admin:angad@clicktock-shard-00-00-2c1tt.mongodb.net:27017,clicktock-shard-00-01-2c1tt.mongodb.net:27017,clicktock-shard-00-02-2c1tt.mongodb.net:27017/test?ssl=true&replicaSet=ClickTock-shard-0&authSource=admin&retryWrites=true")
@@ -12,18 +11,18 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', "*");
-//   res.header(
-//     'Access-Control-Allow-Headers', 
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT, PATCH, POST, DELETE, GET")
-//     return res.status(200).json({});
-//   }
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use('/tasks', taskRouter);
 
